dc14657936333143854e3d7c7622eeb9f7700294
reding <709307374@qq.com>
2015-08-20 14:50:54 +0800

    TODO: 016

--- 015/015/015.cpp
+++ 015/015/015.cpp
@@ -43,6 +43,48 @@ public:
 			}
 		}
 	}
+
+	//two pointers
+	vector<vector<int>> threeSum2(vector<int>& nums)
+	{
+		vector<vector<int>> results;
+		sort(nums.begin(), nums.end());
+		for (int i = 0;i < nums.size();i++)
+		{
+			if (i > 0 && nums[i - 1] == nums[i])
+				continue;
+			int left = i+1;
+			int right = nums.size() - 1;
+			while (left < right)
+			{
+				int sum = nums[i] + nums[left] + nums[right];
+				if (sum > 0)
+				{
+					right--;		
+				}
+				else if (sum < 0)
+				{
+					left++;
+				}
+				else
+				{
+					vector<int> r;
+					r.push_back(nums[i]);
+					r.push_back(nums[left]);
+					r.push_back(nums[right]);
+					results.push_back(r);
+					left++;
+					while (nums[left - 1] == nums[left]&&left<right)
+						left++;
+					while ((right + 1)<nums.size()&&nums[right + 1] == nums[right] && left<right)
+						right--;
+				}
+			}
+
+		}
+		return results;
+	}
+
 };
 
 void test1(Solution s)
@@ -51,7 +93,7 @@ void test1(Solution s)
 	test.push_back(1);
 	test.push_back(-1);
 	test.push_back(0);
-	vector<vector<int>> result = s.threeSum(test);
+	vector<vector<int>> result = s.threeSum2(test);
 	int resultnum = 1;
 	if (result.size() != resultnum) 
 	{
@@ -79,10 +121,46 @@ void test1(Solution s)
 	cout << "test1 passed!" << endl;
 }
 
+void test2(Solution s)
+{
+	vector<int> test;
+	test.push_back(0);
+	test.push_back(0);
+	test.push_back(0);
+	test.push_back(0);
+	vector<vector<int>> result = s.threeSum2(test);
+	int resultnum = 1;
+	if (result.size() != resultnum)
+	{
+		cout << "test 2 failed!" << endl;
+		return;
+	}
+
+	for (int i = 0;i < resultnum;i++)
+	{
+		if (result[i].size() != 3)
+		{
+			cout << "test 2 failed!" << endl;
+		}
+		for (int j = 0;j < 3;j++)
+		{
+			if ((result[i][0] + result[i][1] + result[i][2]) == 0)
+				continue;
+			else
+			{
+				cout << "test 2 failed!" << endl;
+				return;
+			}
+		}
+	}
+	cout << "test2 passed!" << endl;
+}
+
 int main()
 {
 	Solution s;
 	test1(s);
+	test2(s);
 	system("pause");
     return 0;
 }
--- 016/016.cpp
+++ 016/016.cpp
@@ -1,11 +1,76 @@
 // 016.cpp : 定义控制台应用程序的入口点。
 //
 
-#include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <limits>
 
+using namespace std;
+
+class Solution {
+public:
+	int threeSumClosest(vector<int>& nums, int target) {
+		int differ = INT_MAX;
+		int result = 0;
+		sort(nums.begin(), nums.end());
+		for (int i = 0;i < nums.size();i++)
+		{
+			if (i > 0 && nums[i - 1] == nums[i])
+				continue;
+			int left = i + 1;
+			int right = nums.size() - 1;
+			while (left < right)
+			{
+				int sum = nums[i] + nums[left] + nums[right];
+				if (differ >= abs(sum - target))
+				{
+					differ = abs(sum - target);
+					result = sum;
+					left++;
+					while (nums[left - 1] == nums[left] && left<right)
+						left++;
+					while ((right + 1)<nums.size() && nums[right + 1] == nums[right] && left<right)
+						right--;
+				}
+				if (differ < abs(sum - target))
+				{
+					if (sum - target>0)
+					{
+						right--;
+					}
+					else
+					{
+						left++;
+					}
+				}
+			}
+		}
+		return result;
+	}
+};
+
+void test1(Solution s)
+{
+	vector<int> nums;
+	nums.push_back(-1), nums.push_back(2), nums.push_back(1), nums.push_back(4);
+	int target = 1;
+	int result = 2;
+	if (result == s.threeSumClosest(nums, target))
+	{
+		cout << "test 1 passed!" << endl;
+	}
+	else
+	{
+		cout << "test 1 failed!" << endl;
+	}
+}
 
 int main()
 {
+	Solution s;
+	test1(s);
+	system("pause");
     return 0;
 }
 
